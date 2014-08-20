83f00b46b10ad75743616cb35898e29c6627529e
filip nikolovski <filip19_nik@hotmail.com>
2014-08-20 22:22:02 +0200

    First commit

---
+++ Diplomska/ListingSW.aspx.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Diplomska
+{
+    public partial class ListingSW : System.Web.UI.Page
+    {
+        static string connString = "SERVER=localhost;DATABASE=naucen_trud;UID=root;PWD=filip;Allow Zero Datetime=True;";
+        MySqlConnection conn = new MySqlConnection(connString);
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+             if (Session["New"] == null)
+            {
+                Response.Redirect("Login.aspx");
+            }
+            else
+            {
+                if (Session["New"].ToString() == "admin@finki.com")
+                {
+                    lblLoged.Text = "You are logged in as ";
+                    lblLogedAs.Text = Session["New"].ToString() + " ";
+                    if (!IsPostBack)
+                    {
+
+
+
+                        try
+                        {
+                            
+
+                            //DropDownList corrw
+                            string selectCorrAut = "SELECT id, CONCAT(name ,' ',surname) as fullName, email  from user_info WHERE email NOT LIKE ?email";
+                            MySqlCommand cmd11 = new MySqlCommand(selectCorrAut, conn);
+                            cmd11.Parameters.Add("?email", "admin@finki.com");
+                            MySqlDataAdapter adapter11 = new MySqlDataAdapter(cmd11);
+                            DataSet ds11 = new DataSet();
+                            adapter11.Fill(ds11);
+
+                            ddlCorrAut.DataSource = ds11;
+                            ddlCorrAut.DataBind();
+
+                            //GridView code
+                            string sqlSelect = "SELECT sw.id, sw.title, concat( sw.autores,' ',sw.Corresponding_autor) as autors , sw.price, sw.date FROM science_work sw, created c, user_info u WHERE   sw.id=c.science_work_ID  and c.user_info_ID=u.id";
+                            MySqlCommand cmd1 = new MySqlCommand(sqlSelect, conn);
+
+                            MySqlDataAdapter adapter1 = new MySqlDataAdapter(cmd1);
+                            DataSet ds1 = new DataSet();
+                            adapter1.Fill(ds1);
+
+                            gvScienceWork.DataSource = ds1;
+                            gvScienceWork.DataBind();
+                        }
+                        catch (Exception ex)
+                        {
+                            lblErr.Visible = true;
+                            lblErr.Text += ex.ToString();
+                        }
+                        finally
+                        {
+                            conn.Close();
+                        }
+                    }
+                }
+                else
+                {
+
+                    Response.Redirect("Login.aspx");
+                }
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+
+                if (ddlCorrAut.SelectedValue == "0" )
+                {
+                          
+                    string sqlSelectUsr = "SELECT sw.id, sw.title,concat( sw.autores,' ',  sw.Corresponding_autor) as autors, sw.price, sw.date FROM science_work sw, created c, user_info u WHERE   sw.id=c.science_work_ID  and c.user_info_ID=u.id ";
+                    MySqlCommand cmd2 = new MySqlCommand(sqlSelectUsr, conn);
+                   
+                    MySqlDataAdapter adapter1 = new MySqlDataAdapter(cmd2);
+                    DataSet ds1 = new DataSet();
+                    adapter1.Fill(ds1);
+
+                    gvScienceWork.DataSource = ds1;
+                    gvScienceWork.DataBind();
+
+
+
+                }
+                else {
+
+                    string sqlSelectUsr = "SELECT distinct sw.id, sw.title, concat( sw.autores,' ',sw.Corresponding_autor) as autors, sw.price, sw.date FROM science_work sw, created c, user_info u WHERE   sw.id=c.science_work_ID AND c.user_info_ID=u.id AND autores like '%" + ddlCorrAut.SelectedValue + "%' OR Corresponding_autor like '%" + ddlCorrAut.SelectedValue + "%' and sw.id=c.science_work_ID AND c.user_info_ID=u.id ";
+                    MySqlCommand cmd2 = new MySqlCommand(sqlSelectUsr, conn);
+               
+                    MySqlDataAdapter adapter1 = new MySqlDataAdapter(cmd2);
+                    DataSet ds1 = new DataSet();
+                    adapter1.Fill(ds1);
+
+                    gvScienceWork.DataSource = ds1;
+                    gvScienceWork.DataBind();
+                }
+               
+               
+            }
+            catch (Exception ex)
+            {
+                lblErr.Visible = true;
+                lblErr.Text = ex.ToString();
+
+            }
+            finally {
+                conn.Close();
+            }
+        }
+
+      
+
+
+        //delete gridview row
+        protected void button_click(object sender, EventArgs e)
+        {
+            Button ibtn1 = sender as Button;
+            int rowIndex = Convert.ToInt32(ibtn1.Attributes["RowIndex"]);
+
+            try
+            {
+
+                conn.Open();
+
+                string sqlDelete = "DELETE FROM  science_work WHERE id =@id";
+                MySqlCommand cmd = new MySqlCommand(sqlDelete, conn);
+
+                cmd.Parameters.AddWithValue("@id", Convert.ToInt32(rowIndex.ToString()));
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                //lblEx.Text = ex.ToString();
+            }
+            finally
+            {
+                conn.Close();
+                Response.Redirect("ListingSW.aspx");
+            }
+
+
+        }
+
+
+
+          protected void button11_click(object sender, EventArgs e)
+          {
+              Button ibtn11 = sender as Button;
+              int rowIndex = Convert.ToInt32(ibtn11.Attributes["RowIndex"]);
+
+              Session["sessi"] = rowIndex;
+              Response.Redirect("VerziiAdmin.aspx");
+
+          }
+
+
+          //gvScienceWork_RowCreated
+          protected void gvScienceWork_RowCreated(object sender, GridViewRowEventArgs e)
+          {
+              if (e.Row.RowType == DataControlRowType.DataRow)
+              {
+                  e.Row.Attributes["onmouseover"] = "this.style.background='#D1DDF1';";
+
+                  if ((e.Row.RowIndex % 2) == 0)  // if even row
+                      e.Row.Attributes["onmouseout"] = "this.style.background='#EFF3FB';";
+                  else  // alternate row
+                      e.Row.Attributes["onmouseout"] = "this.style.background='White';";
+                  }
+          }
+
+          protected void btnLoguot_Click(object sender, EventArgs e)
+          {
+              Session["New"] = null;
+              Response.Redirect("Login.aspx");
+          }
+    }
+}
---
+++ Diplomska/obj/Release/Package/PackageTmp/RessetPassword.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="RessetPassword.aspx.cs" Inherits="Diplomska.RessetPassword" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+   <title></title>
+     <link href="style/style.css" rel="stylesheet" />
+     <link rel="shortcut icon" href="../Images/favicon.ico" />
+    <link href="style/loginStile.css" rel="stylesheet" />
+    <link href="style/bootstrap.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="container">
+            <div class="container">
+             <div id="acount" style="margin-bottom:7%">
+                 <a href="Index.aspx" style="float:left">
+                    <asp:Image ID="imgLogo" runat="server" ImageUrl="~/Images/ukim-logo-9.png" /><asp:Image ID="imgLogo1" runat="server" ImageUrl="~/Images/finki-logo-9.png" /></a>
+               
+               
+
+            </div>
+            <div class="main-content" style="padding-left: 3%">
+                <div class="main-content1">
+                <h3 class="text-muted">
+                    <asp:Label ID="lblTitle1" runat="server" Text="Рестарирање на лозинка."></asp:Label></h3>
+
+                <table>
+                    <tr>
+                        <td>
+                            <asp:Label ID="lblEmail" runat="server" Text="Внеси емаил"></asp:Label></td>
+                        <td>
+                            <asp:TextBox ID="txtEmail" runat="server" class="form-control" placeholder="емаил"></asp:TextBox></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            
+                        </td>
+                        <td>
+                            
+                            <asp:Button ID="btnCancel" runat="server" Text="cancel" class="btn btn-primary" OnClick="btnCancel_Click"/>
+                            <asp:Button ID="btnSend" runat="server" Text="испрати барање" class="btn btn-primary" OnClick="btnSend_Click"/>
+                        </td>
+                    </tr>
+                </table>
+
+                    <asp:Label ID="lblErr" runat="server" Text="" ForeColor="Red"></asp:Label>
+                </div>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
